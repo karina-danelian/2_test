6870f3ebe90f8e1d2b86f40bb49cdde23073abdf
Karina <karinin.mailbox@gmail.com>
2024-07-27 21:14:28 +0300

    Initial commit

---
+++ src/main/java/KS1/3.kt
@@ -0,0 +1,18 @@
+package KS1
+
+fun main() {
+
+
+   val year = "1961"
+   var hour = "9"
+   var minute = "07"
+    //println(year)
+    //println(hour)
+    //println(minute)
+    hour = " 10"
+    minute = " 55"
+    print(year)
+    print(hour)
+    print(minute)
+
+}
