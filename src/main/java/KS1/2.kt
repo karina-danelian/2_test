0d275348b6230d6883621ec3df10c5871509bd79
Karina <karinin.mailbox@gmail.com>
2024-07-27 21:17:37 +0300

    Initial commit

---
+++ src/main/java/KS1/2.kt
@@ -0,0 +1,17 @@
+package KS1
+fun main() {
+
+    val numberOfOrders: Byte = 75
+
+    val textForVisitors = "Спасибо за покупку!"
+
+    println (textForVisitors)
+    println (numberOfOrders)
+
+
+    var numberOfstaff = 2000
+    //println (numberOfstaff)
+    numberOfstaff = 1999
+    println (numberOfstaff)
+
+}
